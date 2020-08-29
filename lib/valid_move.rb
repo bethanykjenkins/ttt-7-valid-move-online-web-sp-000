1b74cfc69d2932317ffbd8ac2db4e8798d242e46
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-08-29 22:15:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,39 +1,19 @@
 # code your #valid_move? method here
 
-#def valid_move?(board, index)
- # position = index.to_i + 1
- #if position_taken?(board,index) && position.between?(0, 8)
-   # false 
- # else
- #   true
-#  end
-#end
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-
-#def position_taken? (board, index)
- # if board[index] == " " || board[index] == "" || board #[index] == nil
- #  taken = false
- # else
-  #  taken = true
- # end
-  
-#end
-
-
-# code your #valid_move? method here
-def valid_move?(board,index)
-  if position_taken?(board, index) != true && index.between?(0,8)
-    true
+def valid_move?(board, index)
+  position = index.to_i + 1
+ if position_taken?(board,index) && position.between?(0, 8)
+    false 
   else
-    false
+    true
   end
 end
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board[index] == "" || board[index] == " " || board[index] == nil
-    false
-  elsif board[index] == "O" || board[index] == "X"
-    true
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board [index] == nil
+   taken = false
+  else
+    taken = true
   end
 end
